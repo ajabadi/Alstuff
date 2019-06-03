22e5eea4a3b1567f91dcccc4c5f3ff4ab4f4f8e4
Al <ajalalabadi@unimelb.edu.au>
2019-06-03 13:14:03 +1000

    modify documentation

--- R/Addins.R
+++ R/Addins.R
@@ -2,14 +2,19 @@
 #'
 #' Go to Tools > 'Modify Keybaord Shortcuts' toadd custom shortcuts after installing it.
 #'
-#' @references \url{https://rstudio.github.io/rstudioaddins/}
-#' @importFrom rstudioapi insertText
-#' @rdname Addins
+#'@references \url{https://rstudio.github.io/rstudioaddins/}
+#'@importFrom rstudioapi insertText
+#'@rdname Addins
+#'@usage {(Add to RStudio keyboard shortcuts)}
 #'@export
 insertInAddin <- function() {
   insertText(" %in% ")
 }
 
+
+#'@importFrom rstudioapi insertText
+#'@rdname Addins
+#'@usage {(Add to RStudio keyboard shortcuts)}
 #'@export
 insert2wayPipe <- function() {
   insertText(" %<>%  .[")
