870994d978352fe77c71316c8631e43bce7ed953
Al <ajalalabadi@unimelb.edu.au>
2019-06-03 13:14:23 +1000

    add manual test

--- roxygenise.R
+++ roxygenise.R
@@ -1,4 +1,4 @@
-setwd('/Users/alabadi/Documents/_Projects/_Personal/someWrappers')
+# setwd('/Users/alabadi/Documents/_Projects/_Personal/someWrappers')
 ############# build all documentation for the package and update namespace
 roxygen2::roxygenise()
 ############# load the pks to access functions
@@ -7,7 +7,6 @@ devtools::load_all()
 testthat::test_package("someWrappers")
 ############# test examples
 ## path to Rd files - will let you know if there are any errors/warnings
-testthat::test_example("man/file_recorder.Rd")
 testthat::test_example("man/parent_base_ext.Rd")
 ############# manual tests
 ## test files that cannot be included in standard testthat pipeline
@@ -16,6 +15,8 @@ invisible(lapply(list.files("tests/manual", full.names = TRUE), source))
 # devtools::check()
 ############# build it
 ## by default, it is made in parent directoy of the package, we manually set it.
+## testhat has changed wd
+setwd('/Users/alabadi/Documents/_Projects/_Personal/someWrappers')
 binary_dir <- "../__binary"
 if(!dir.exists(binary_dir)){
   dir.create(binary_dir)
