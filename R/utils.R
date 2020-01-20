dca2a67f76152961f0e28162d164c6df28062f96
Al J Abadi <al.jal.abadi@gmail.com>
2020-01-20 13:43:51 +1100

    force update by default

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,8 @@
 #' Update the package from github
 #'
+#' @param force Logical, force re-install?
+#'
 #' @export
-update_Altools <- function(){
-  devtools::install_github("ajabadi/Altools", force = TRUE, upgrade = FALSE)
+update_Altools <- function(force = TRUE){
+  devtools::install_github("ajabadi/Altools", force = force, upgrade = FALSE)
 }
