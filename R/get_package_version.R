82cb0f2e06d096070f488e4f74d025f5c58cd0d3
Al J Abadi <al.jal.abadi@gmail.com>
2020-03-30 14:39:44 +1100

    get branch version

--- R/get_package_version.R
+++ R/get_package_version.R
@@ -5,8 +5,15 @@
 #' @return Package's version as character x.y.z
 #' @export
 #' @family gitflow
-get_pkg_version <- function(dir = '.') {
+get_pkg_version <- function(dir = '.', branch = NULL) {
     setwd(dir)
+
+    if (!is.null(branch)) {
+        ## stash changes & unstash upon exit after checkout to original branch
+        system(sprintf('git add .;git stash push -m "prestash"; git checkout %s', branch))
+        on.exit(system(sprintf('git checkout -; git stash pop stash@\\{0\\}')))
+    }
+
     versionLine <- system("awk '/Version/{ print NR; exit }' DESCRIPTION", intern = TRUE)
     versionLine <- as.integer(versionLine)
     currVersion <- read.dcf("DESCRIPTION", fields = "Version")
