1de7b9348cd2d5ae5a66a743b036e95f6edb438a
Al J Abadi <al.jal.abadi@gmail.com>
2020-04-02 16:10:06 +1100

    add function

---
+++ R/eval_function_formals.R
@@ -0,0 +1,24 @@
+#' Evaluate function formals in an environment
+#'
+#' @param fun Name, function name
+#' @param envir environment, in  which to evaluate. (.GlobalEnv by default)
+#' @param ... passed to base::eval
+#'
+#' @return Function formals are evaluated in the specified environment
+#' @export
+#'
+#' @examples
+#' eval_function_formals(rnorm)
+eval_function_formals <- function(fun, envir = .GlobalEnv, ...) {
+  fun_formals <- formals(fun)
+  fun_formals <- formals(fun)
+  fun_formals$... <- NULL ## if any
+  invisible(lapply(X = names(fun_formals), FUN = function(x){
+    cat(sprintf("evaluating %s ...", x))
+    tryCatch({
+      assign(x = x, value = eval(fun_formals[[x]]), envir = envir, ...)
+      cat(sprintf("successful\n", x))
+      },
+             error = function(e) message(sprintf("\nNo default value for '%s'", x)))
+  }))
+}
