0d3f69fb493e4ba0d10577d7d5ec91ab8153edef
Al J Abadi <al.jal.abadi@gmail.com>
2020-05-06 21:57:39 +1000

    reproducible rand_na only if seed != NULL

--- R/rand_na.R
+++ R/rand_na.R
@@ -1,15 +1,21 @@
 #' Random NA inseration
 #'
-#' @param mat a matrix, or data.frame
-#' @param prop proportion of NAs
+#' @param mat Matrix, or data.frame of data
+#' @param prop Numeric b/w 0 and 1, proportion of NAs
+#' @param seed Integer, seed number
 #'
 #' @return matrix with NAs
 #' @export
 #'
 #' @examples
 #' rand_na()
-rand_na <- function(mat=matrix(1:100, ncol = 20), prop=0.3){
-  set.seed(42)
+rand_na <- function(mat=matrix(1:100, ncol = 20), prop=0.3, seed = NULL){
+
+  if (!is.null(seed))
+  {
+    RNGversion('3.6.5')
+    set.seed(seed)
+  }
   ## calculate the total number of NAs
   total_na <- floor(prop*prod(dim(mat)))
   vec <- as.vector(mat)
