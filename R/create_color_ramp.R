5d79c605e4fe0678ce940a50a8d302d678f331d7
Al J Abadi <al.jal.abadi@gmail.com>
2020-05-29 10:30:09 +1000

    automatic push of utility updates

--- R/create_color_ramp.R
+++ R/create_color_ramp.R
@@ -2,17 +2,21 @@
 #'
 #' @param col_range Colors to transition from/to
 #' @param n Number of colors to create
+#' @param plot whether to plot the colors
 #'
 #' @return A visual of colors and a character vector of colors
 #' @export
 #'
 #' @examples
 #' create_color_ramp(c('#a4eaff', '#0a5090'))
-create_color_ramp <- function(col_range = c("red","yellow","springgreen","royalblue"), n = 30) {
+create_color_ramp <- function(col_range = c("red","yellow","springgreen","royalblue"), n = 30, plot = TRUE) {
 
   cols <- colorRampPalette(col_range)(n)
-  plot(x=seq_len(n), y = sin(seq_len(n)*pi*((length(col_range)-1))/n),
-       col = cols, pch=19, cex=2, xlab = '', ylab = '', axes = FALSE)
+  if (plot) {
+    plot(x=seq_len(n), y = sin(seq_len(n)*pi*((length(col_range)-1))/n),
+         col = cols, pch=19, cex=2, xlab = '', ylab = '', axes = FALSE)
+  }
+
 
   return(cols)
 }
