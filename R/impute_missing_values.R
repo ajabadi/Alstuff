7a91ef0a78b9140d657aaf95b8eee4cb98600556
Al J Abadi <al.jal.abadi@gmail.com>
2020-10-19 15:56:55 +1100

    add impute_missing_values

---
+++ R/impute_missing_values.R
@@ -0,0 +1,37 @@
+#' Impute missing values om columns by means/median
+#'
+#' For type = 'mean' por 'median', the corresponding values for columns
+#' are returned. For type = 'knn', \code{impute::knn} is used.
+#' @param x A numeric matrix - features are columns
+#' @param type One of c('mean', 'median', 'knn')
+#' @param ... arguments passed to \code{impute::knn}
+#'
+#' @return Imputed x
+#'
+#' @examples
+#' impute_by_means(matrix(c(1,2,3, NA,
+#'                          4,5,6, NA), byrow = FALSE, ncol=2))
+#' @export
+impute_missing_values <- function(x, type = c('mean', 'median', 'knn'), ...) {
+  if (!is.matrix(x)) {
+    stop("'x' must be a numeric matrix")
+  }
+
+  type <- match.arg(type)
+  ind.na <- which(is.na(x), arr.ind = TRUE)
+
+  if (type == 'mean')
+  {
+    replacements <- colMeans(x, na.rm = TRUE)
+  } else if (type == 'median')
+  {
+    replacements <- apply(x, 2, median)
+  } else if (type == 'knn')
+  {
+    return(t(impute::impute.knn(t(x), ...)$data))
+  }
+
+  x[ind.na] <- replacements[ind.na[,'col']]
+
+  return(x)
+}
